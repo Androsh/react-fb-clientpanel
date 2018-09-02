177f9634b8ac2f3611b03d4a7ed33a540ac95211
androsh <apanasiuk00@gmail.com>
2018-09-02 17:48:04 +0100

    Add client to firestore

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import AppNavbar from './components/layout/AppNavbar';
 import Dashboard from './components/layout/Dashboard';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
 import store from './store';
+import AddClient from './components/clients/AddClient';
 
 class App extends Component {
   render() {
@@ -17,6 +17,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Dashboard} />
+                <Route exact path="/client/add" component={AddClient} />
               </Switch>
             </div>
           </div>
